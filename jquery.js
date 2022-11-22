56d9c037d344b4aee6645d52b5b44a5931f4075a
Muhammad Zain <114310149+Muhammad-Zain-Yaseen@users.noreply.github.com>
2022-11-22 05:49:40 -0600

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,10 @@
+
+$("#Reservation").click(function(){
+alert("The paragraph was clicked.");
+});
+
+
+$("#History").click(function(){
+    console.log('Is it working: ')
+alert("The paragraph was clicked.");
+});
